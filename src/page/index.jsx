7d484101859955314c960de954961b159db623d6
KhaledBenAbderrahim <khaled.ben@gmx.de>
2024-11-16 10:26:00 +0100

    csrd
    csrd

---
+++ src/page/index.jsx
@@ -0,0 +1,340 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='desktop-header'>
+        <div className='hero'>
+          <div className='navbar'>
+            <div className='logo'>
+              <div className='rectangle' />
+            </div>
+            <div className='button'>
+              <div className='text'>
+                <span className='login'>Login</span>
+              </div>
+            </div>
+            <div className='menu'>
+              <div className='frame'>
+                <div className='link'>
+                  <span className='home'>Home</span>
+                </div>
+                <div className='link-1'>
+                  <span className='product'>Product</span>
+                </div>
+                <div className='link-2'>
+                  <span className='about'>About</span>
+                </div>
+                <div className='link-3'>
+                  <span className='contact'>Contact</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='confetti-shapes'>
+            <div className='vector' />
+            <div className='vector-4' />
+          </div>
+          <div className='text-5'>
+            <div className='headline'>
+              <span className='lightning-fast-prototyping'>
+                Lightning fast <br />
+                prototyping
+              </span>
+            </div>
+            <div className='sub-heading'>
+              <span className='freelancer-calendar'>
+                Most calendars are designed for teams. Slate is <br />
+                designed for freelancers.
+              </span>
+            </div>
+          </div>
+          <div className='buttons'>
+            <div className='button-6'>
+              <div className='text-7'>
+                <span className='get-started'>Get Started</span>
+              </div>
+            </div>
+            <div className='button-8'>
+              <div className='text-9'>
+                <span className='try-for-free'>Try For Free</span>
+              </div>
+            </div>
+          </div>
+          <div className='screen'>
+            <div className='flat-browser' />
+          </div>
+        </div>
+      </div>
+      <div className='light-cta'>
+        <div className='content'>
+          <div className='text-content'>
+            <div className='title'>
+              <span className='just-type'>Just type</span>
+            </div>
+            <div className='second-headline'>
+              <span className='fastest-way-to-navigate'>
+                Fastest way to navigate
+              </span>
+            </div>
+            <div className='sub-heading-a'>
+              <span className='most-calendars-designed'>
+                Most calendars are designed for teams. <br />
+                Slate is designed for freelancers.
+              </span>
+            </div>
+          </div>
+          <div className='buttons-b'>
+            <div className='button-c'>
+              <div className='text-d'>
+                <span className='download'>Download</span>
+              </div>
+            </div>
+            <div className='button-e'>
+              <div className='text-f'>
+                <span className='see-walkthrough'>See Walkthrough</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='light-content'>
+        <div className='content-10'>
+          <div className='text-content-11'>
+            <div className='title-12'>
+              <span className='opentype-features'>OpenType features </span>
+            </div>
+            <div className='most-calendars-designed-13'>
+              <span className='learn-more'>
+                Most calendars are designed for teams. <br />
+                Slate is designed for freelancers who <br />
+                want a simple way to plan their <br />
+                schedule.
+              </span>
+            </div>
+            <div className='link-14'>
+              <span className='learn-more-15'>LEARN MORE</span>
+            </div>
+          </div>
+          <div className='frame-16'>
+            <div className='frame-17'>
+              <div className='vector-18' />
+            </div>
+            <div className='screen-19' />
+          </div>
+        </div>
+      </div>
+      <div className='light-content-1a'>
+        <div className='content-1b'>
+          <div className='frame-1c'>
+            <div className='frame-1d'>
+              <div className='vector-1e' />
+            </div>
+            <div className='screen-1f' />
+          </div>
+          <div className='text-content-20'>
+            <div className='title-21'>
+              <span className='variable-fonts'>Variable fonts</span>
+            </div>
+            <div className='sub-heading-22'>
+              <span className='team-slate-freelancers'>
+                Most calendars are designed for <br />
+                teams. Slate is designed for <br />
+                freelancers
+              </span>
+            </div>
+            <div className='link-23'>
+              <span className='learn-more-24'>LEARN MORE</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='light-cta-25'>
+        <div className='content-26'>
+          <div className='text-content-27'>
+            <div className='second-headline-28'>
+              <span className='rethink-work'>A rethink of how we work</span>
+            </div>
+            <div className='paragraphe'>
+              <span className='rollercoaster-emotions'>
+                Being a freelancer is a rollercoaster of emotions. We built
+                Slate to keep your <br />
+              </span>
+            </div>
+          </div>
+          <div className='progress-bar-title'>
+            <div className='progress-bar' />
+            <div className='flex-row-c'>
+              <span className='facebook'>Facebook</span>
+              <span className='digits'>35,929</span>
+            </div>
+          </div>
+          <div className='button-29'>
+            <div className='get-started-free'>
+              <span className='light-testimonial'>Get Started For Free</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='frame-2a'>
+        <div className='vector-2b'>
+          <div className='vector-2c' />
+          <div className='title-2d' />
+        </div>
+        <div className='testimonials'>
+          <span className='brands'>Testimonials</span>
+        </div>
+        <div className='logos-ibm'>
+          <div className='vector-2e'>
+            <div className='paragraphe-2f' />
+          </div>
+        </div>
+        <div className='freelance-business-stressful'>
+          <span className='client'>
+            Being a freelancer is a rollercoaster of emotions. <br />
+            We built Slate to keep your freelance business less stressful.
+            <br />
+            We'd love to show you what we're building...
+          </span>
+        </div>
+        <div className='box-14'>
+          <div className='avatar'>
+            <div className='ellipse' />
+          </div>
+          <div className='text-content-30'>
+            <div className='link-31'>
+              <span className='esther-webb'>Esther Webb</span>
+            </div>
+            <div className='subtitle'>
+              <span className='designer'>Designer</span>
+            </div>
+          </div>
+        </div>
+        <div className='button-32'>
+          <div className='text-33'>
+            <span className='all-testimonials'>All Testimonials</span>
+          </div>
+        </div>
+      </div>
+      <div className='dark-pricing'>
+        <div className='content-34'>
+          <div className='text-content-35'>
+            <div className='second-headline-36'>
+              <span className='pricing'>Pricing</span>
+            </div>
+            <div className='paragraphe-37'>
+              <span className='text-content-38'>
+                Most calendars are designed for teams. Slate is <br />
+                designed for freelancers who want a simple <br />
+                way to plan their schedule.
+              </span>
+            </div>
+          </div>
+          <div className='text-content-39'>
+            <div className='subtitle-3a'>
+              <span className='text-21'>30-DAY FREE TRIAL</span>
+            </div>
+            <div className='frame-3b'>
+              <div className='headline-3c'>
+                <span className='dollar'>$5</span>
+              </div>
+              <div className='sub-heading-3d'>
+                <span className='per-month-user'>
+                  /month per <br />
+                  user
+                </span>
+              </div>
+            </div>
+            <div className='paragraphe-3e'>
+              <span className='calendars-teams'>
+                Most calendars are designed <br />
+                for teams.
+              </span>
+            </div>
+            <div className='button-3f'>
+              <div className='text-40'>
+                <span className='get-started-41'>Get Started</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='light-content-42'>
+        <div className='content-43'>
+          <div className='text-block'>
+            <div className='title-44'>
+              <span className='ergonomic-meeting'>
+                We focus on ergonomic
+                <br />
+                meeting
+              </span>
+            </div>
+          </div>
+          <div className='text-row'>
+            <div className='paragraph'>
+              <div className='freelancer-emotions'>
+                <span className='freelancer-emotions-45'>
+                  Being a freelancer is a rollercoaster of emotions. <br />
+                  We built Slate to keep your freelance business
+                  <br />
+                  building...
+                  <br />
+                  <br />
+                </span>
+                <span className='freelancer-emotions-46'>
+                  Being a freelancer is a rollercoaster of emotions. <br />
+                  We built Slate to keep your freelance business
+                  <br />
+                  building...
+                </span>
+              </div>
+            </div>
+            <div className='button-47'>
+              <div className='text-48'>
+                <span className='get-started-49'>Get Started</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='light-footer'>
+        <div className='brand'>
+          <div className='logo-4a'>
+            <div className='rectangle-4b' />
+          </div>
+          <div className='subtitle-4c'>
+            <span className='on-your-mind'>Just type what's on your mind</span>
+          </div>
+        </div>
+        <div className='social-media'>
+          <div className='twitter-outlined'>
+            <div className='vector-4d' />
+          </div>
+          <div className='facebook-filled'>
+            <div className='vector-4e' />
+          </div>
+          <div className='linkedin-filled'>
+            <div className='vector-4f' />
+          </div>
+        </div>
+        <div className='menu-50'>
+          <div className='frame-51'>
+            <div className='link-52'>
+              <span className='home-53'>Home</span>
+            </div>
+            <div className='link-54'>
+              <span className='product-55'>Product</span>
+            </div>
+            <div className='link-56'>
+              <span className='about-57'>About</span>
+            </div>
+            <div className='link-58'>
+              <span className='contact-59'>Contact</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
